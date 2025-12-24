c8dc0a1b32ad88674076e6e73246b9c111eba1fa
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-12-24 20:10:32 +0530

    agent updated

--- Installations/jenkins.sh
+++ Installations/jenkins.sh
@@ -13,8 +13,10 @@ sudo systemctl daemon-reload
 
 
 #increase the memort for the node-agent
-sudo growpart /dev/nvme0n1p 4
+sudo growpart /dev/nvme0n1 4
 sudo lvextend -L +15G /dev/mapper/RootVG-homeVol   #home folder
 sudo xfs_growfs /home
 sudo lvextend -L +15G  /dev/mapper/RootVG-rootVol  # root folder
 sudo xfs_growfs /
+
+sudo lvextend -L +15G /dev/mapper/RootVG-varVol
